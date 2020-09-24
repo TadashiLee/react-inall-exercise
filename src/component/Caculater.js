d134e8cb47a7cf543abfe5589692301d0c45c592
TadashiLee <584638605@qq.com>
2020-09-24 23:20:40 +0800

    [wenchang.li]:finish the caculater and timer and css

--- src/component/Caculater.js
+++ src/component/Caculater.js
@@ -1,14 +1,121 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import Button from './Button';
+import Text from './Text';
+import '../css/Caculater.css'
 
 class Caculater extends Component {
+  
+    state={
+        string:""
+    }
+
+handleButton=(e)=>{
+    if (e.target.value!==undefined){
+        let input=e.target.value;
+        let prvcontent=this.state.string;
+        let content="";
+        if (input==="+"||input==="-"||input==="*"||input==="/"){
+            content=prvcontent+" "+input+" ";
+        }
+        else if (input==="Clear"){
+            content="";
+        }
+        else if (input==="="){
+            let arr=prvcontent.split(" ");
+            let ans=[];
+            let i=0;
+            while (i<arr.length){
+                if (arr[i]==="")
+                    i++;
+                else if (arr[i]==="+"){
+                    ans.push(arr[i+1]);
+                    i+=2;
+                }
+                else if (arr[i]==="-"){
+                    ans.push(-arr[i+1]);
+                    i+=2;
+                }
+                else if (arr[i]==="*"){
+                    let a;
+                    let b=ans.pop();
+                    if(arr[i+1]==="-"){
+                        a=-arr[i+2];
+                        i+=3;
+                    }
+                    else {
+                        a=arr[i+1];
+                        i+=2;
+                    }
+                    ans.push(b*a);
+                }
+                else if (arr[i]==="/"){
+                    let a;
+                    let b=ans.pop();
+                    if (arr[i+1]==="0"){
+                        content="ERROR!";
+                        return;
+                    }
+                    else if(arr[i+1]==="-"){
+                        a=-arr[i+2];
+                        i+=3;
+                    }
+                    else {
+                        a=arr[i+1];
+                        i+=2;
+                    }
+                    ans.push(b/a);
+                }
+                else {
+                    ans.push(arr[i]);
+                    i++;
+                }
+
+            }
+            // console.log(ans);
+            let fin_ans=parseFloat(ans[0]);
+            for (i=1;i<ans.length;i++)
+                fin_ans+=parseFloat(ans[i]);
+            content=fin_ans;
+        }
+        else{
+            content=prvcontent+input;
+        }
+        this.setState({
+            string:content
+        })
+    }
+}
+  
   render() {
     return (
-      <div>
-        <p>This is a Caculater</p>
-        <NavLink exact to='/' className='nav-link'activeStyle={{fontWeight: "bold",color: "red"}}>
-          回到主页
-        </NavLink>
+      <div id="main">
+                <div id="input_text">
+                    <Text value={this.state.string}/>
+                </div>
+                <div id="input_button" onClick={this.handleButton}>
+                    <Button value={1}/>
+                    <Button value={2}/>
+                    <Button value={3} />
+                    <Button value={"+"} className={"js_size"}/>
+                    <Button value={"-"}  className={"js_size"}/>
+                    <Button value={4}/>
+                    <Button value={5}/>
+                    <Button value={6}/>
+                    <Button value={"*"} className={"js_size"}/>
+                    <Button value={"/"} className={"js_size"}/>
+                    <Button value={7}/>
+                    <Button value={8}/>
+                    <Button value={9} />
+                    <Button value={0}/>
+                    <Button value={"Clear"} className={"clear_size"}/>   
+                    <Button value={"="} className={"equal_size"}/>
+        </div>
+        <div>
+            <NavLink exact to='/' className='nav-link'activeStyle={{fontWeight: "bold",color: "red"}}>
+              回到主页
+           </NavLink>
+        </div>    
       </div>
     );
   }
--- src/component/Timer.js
+++ src/component/Timer.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import Text from './Text';
 class Timer extends Component {
+  state = {
+    time: 0,
+    view: "start",
+  };
+  time = () => {
+    // 清除可能存在的定时器
+    clearInterval(this.timer)
+    // 创建（重新赋值）定时器
+    this.timer = setInterval(() => {
+      // 当前时间回显-1
+      this.setState({
+        time: this.state.time - 1,
+        view: this.state.time - 1 + " seconds",
+      }, () => {
+        // 判断修改后时间是否小于1达到最小时间
+        if (this.state.time <= 0) {
+          // 清除定时器
+          this.setState({view:"End",});
+          clearInterval(this.timer);
+          // 结束定时器循环
+          return;
+        }
+        // 循环自调用
+          this.time();
+      });
+    }, 1000);
+  }
+  componentWillUnmount(){
+    clearInterval(this.timer)
+  }
+
+  handleTimeChange = (event)=>{
+    this.setState({
+      time: event.target.value,
+      view:"Start",
+    });
+  };
+  
   render() {
     return (
       <div>
-        <p>This is a Timer.</p>
+        <div id="content">
+            <input type="text" value={this.state.time} onChange={(e)=>this.handleTimeChange(e)}/>
+        </div>
+        <div id="view">
+            <h1>{this.state.view}</h1>
+        </div>
+        <div>
+          <input type="button" className="setTime" value="开始计时" onClick={this.time}/>
+        </div>
         <NavLink exact to='/' className='nav-link'activeStyle={{fontWeight: "bold",color: "red"}}>
           回到主页
         </NavLink>
